ffb8ac24214118a546454942ef01a4cbf5a48496
hallelhel <hallelh@post.bgu.ac.il>
2021-05-27 21:21:22 +0300

    search by name

--- project/routes/team.js
+++ project/routes/team.js
@@ -7,8 +7,13 @@ const teams_utils = require("./utils/teams_utils");
 router.get("/fullInfo/:teamID", async (req, res, next) => {
   let team_details = [];
   try {
+    //logo and name
+    const team_name_logo = await teams_utils.getTeambasicDetailsByID(req.params.teamID);
+    team_details.push(team_name_logo);
+    ////players of team include names,pic,number of thripies
     const team_players_details = await players_utils.getPlayersByTeam(req.params.teamID);
     team_details.push(team_players_details);
+    //teams coach
     const coach = await teams_utils.getCoach(req.params.teamID)
     team_details.push(coach);
     //const team_past_games_by_Season_ID = await 
@@ -20,9 +25,17 @@ router.get("/fullInfo/:teamID", async (req, res, next) => {
   }
 
 });
-router.get("/basicInfo/:teamName", async(req, res, next) =>{    
+router.get("/basicInfoName/:teamName", async(req, res, next) =>{    
   try {
       let team_det = await teams_utils.getTeambasicDetailsByName(req.params.teamName);
+      res.send(team_det);
+  } catch (error) {
+      next(error);
+  }
+});
+router.get("/basicInfoID/:teamID", async(req, res, next) =>{    
+  try {
+      let team_det = await teams_utils.getTeambasicDetailsByID(req.params.teamID);
       res.send(team_det);
   } catch (error) {
       next(error);
--- project/routes/utils/teams_utils.js
+++ project/routes/utils/teams_utils.js
@@ -27,14 +27,16 @@ async function getCoach(team_id) {
     let teams_info = await Promise.all(promises);
     return extractRelevantTeamData(teams_info);
   }
+  
   function extractRelevantTeamData(teams_info) {
     return teams_info.map((team_info) => {
-      const { name , short_code } = team_info.data.data;
+      const { name , short_code, logo_path } = team_info.data.data;
       
       //const { name } = team_info.data.data.country.data;
       return {
         name: name,
         short_name: short_code,
+        // image: logo_path,
         //country: name,  
       };
     });
@@ -61,8 +63,25 @@ async function getCoach(team_id) {
         }
       })
     }
-
-
+    async function getTeambasicDetailsByID(team_ID){
+      const team_info = await axios.get(`${api_domain}/teams/${team_ID}`, {
+        params: {
+          api_token: process.env.api_token,
+          include: "league", 
+          },
+        })
+          const { name , logo_path } = team_info.data.data;
+          const {id} = team_info.data.data.league.data
+          if(id == 271){
+            return {
+              name: name,
+              image: logo_path,
+                
+            };
+          }
+        
+      }
   exports.getCoach = getCoach;
   exports.getTeamsInfo = getTeamsInfo;
   exports.getTeambasicDetailsByName = getTeambasicDetailsByName;
+  exports.getTeambasicDetailsByID = getTeambasicDetailsByID;
